da0cf9c0e866f63b6e6883c649280bab5cd717c7
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-01-23 00:57:39 +0100

    debug

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -30,7 +30,7 @@ main() {
     SOURCE_DIR=$(dirname $0)
     ZABBIX_DIR=/etc/zabbix
 
-    cp -rv ${SOURCE_DIR}/custix/fail2ban.conf   ${ZABBIX_DIR}/zabbix_agentd.d/fail2ban.conf
+    cp -rv ${SOURCE_DIR}/fail2ban.conf ${ZABBIX_DIR}/zabbix_agentd.d/fail2ban.conf
 
     systemctl restart zabbix-agent.service
 }

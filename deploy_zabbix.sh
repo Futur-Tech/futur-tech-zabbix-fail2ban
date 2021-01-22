1216db41911128d543c8a0758c908382f25e6b0c
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-01-22 23:08:22 +0100

    self update script

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
-SOURCE_DIR=$(dirname $0)
-ZABBIX_DIR=/etc/zabbix
 
-cp -rv ${SOURCE_DIR}/custix/fail2ban.conf   ${ZABBIX_DIR}/zabbix_agentd.d/fail2ban.conf
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTNAME="$0"
+ARGS="$@"
+
+BRANCH="master"
+
+self_update() {
+    cd $SCRIPTPATH
+    git fetch
+
+    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+        echo "Found a new version of me, updating myself..."
+        git pull --force
+        git checkout $BRANCH
+        git pull --force
+        echo "Running the new version..."
+        exec "$SCRIPTNAME" "$@"
+
+        # Now exit this old instance
+        exit 1
+    }
+    echo "Already the latest version."
+}
+
+main() {
+    SOURCE_DIR=$(dirname $0)
+    ZABBIX_DIR=/etc/zabbix
+
+    cp -rv ${SOURCE_DIR}/custix/fail2ban.conf   ${ZABBIX_DIR}/zabbix_agentd.d/fail2ban.conf
+
+    systemctl restart zabbix-agent.service
+}
+
+self_update
+main

cb946a9716c0eee9d7a9b526dda642b5a69d8053
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2022-03-03 21:46:30 +0100

    Update to Zabbix Agent 2

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,21 @@
 
 source "$(dirname "$0")/ft-util/ft_util_inc_var"
 
-ZBX_ETC="/etc/zabbix"
-ZBX_CONF_AGENT="${ZBX_ETC}/zabbix_agentd.conf"
-ZBX_CONF_AGENT_D="${ZBX_ETC}/zabbix_agentd.conf.d"
 SRC_DIR="/usr/local/src"
 SUDOERS_ETC="/etc/sudoers.d/ft-fail2ban"
 
+# Checking which Zabbix Agent is detected and adjust include directory
+$(which zabbix_agent2 >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agent2.d"
+$(which zabbix_agentd >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
+if [ ! -d "${ZBX_CONF_AGENT_D}" ] ; then $S_LOG -s crit -d $S_NAME "${ZBX_CONF_AGENT_D} Zabbix Include directory not found" ; exit 10 ; fi
+
+
 $S_LOG -d $S_NAME "Start $S_NAME $*"
 
-#############################
-#############################
-## SETUP SUDOER FILES
-#############################
-#############################
+echo "
+  SETUP SUDOER FILES
+------------------------------------------"
 
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==== SUDOERS CONFIGURATION ===="
 $S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
 
 echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$SUDOERS_ETC &>/dev/null
@@ -28,23 +27,20 @@ echo "zabbix ALL=(ALL) NOPASSWD:/usr/bin/fail2ban-client status *" | sudo EDITOR
 cat $SUDOERS_ETC | $S_LOG -d "$S_NAME" -d "$SUDOERS_ETC" -i 
 
 $S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
 
-#############################
-#############################
-## INSTALL ZABBIX FILES 
-#############################
-#############################
+
+echo "
+  INSTALL ZABBIX FILES
+------------------------------------------"
 
 $S_DIR_PATH/ft-util/ft_util_file-deploy "$S_DIR/zbx.conf/ft-fail2ban.conf" "${ZBX_CONF_AGENT_D}/ft-fail2ban.conf"
 
-#############################
-#############################
-## RESTART ZABBIX LATER
-#############################
-#############################
 
-echo "service zabbix-agent restart" | at now + 1 min &>/dev/null ## restart zabbix agent with a delay
+echo "
+  RESTART ZABBIX LATER
+------------------------------------------"
+
+echo "systemctl restart zabbix-agent*" | at now + 1 min &>/dev/null ## restart zabbix agent with a delay
 $S_LOG -s $? -d "$S_NAME" "Scheduling Zabbix Agent Restart"
 
 $S_LOG -d "$S_NAME" "End $S_NAME"
